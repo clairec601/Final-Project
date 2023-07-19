2a4523b495680f1ee393efbb985880a50079803d
Yoselin <yoselincastillo125@gmail.com>
2023-07-19 10:46:51 -0400

    I just changed the text to -- Welcome to Productivi-Me

--- Final Project/ContentView.swift
+++ Final Project/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("this is our final project")
+            Text("Welcome to Productivi-Me")
         }
         .padding()
     }
