dc4574532b82199d44192a7753963d343590e3f0
Yoselin <yoselincastillo125@gmail.com>
2023-07-19 19:35:00 -0400

    I made a beginning page for the app, I am starting on the quiz

--- Final Project/ContentView.swift
+++ Final Project/ContentView.swift
@@ -9,15 +9,52 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("Welcome to Productivi-Me")
-                .font(.title2)
-                .fontWeight(.black)
-                .foregroundColor(Color("AccentColor5"))
+        NavigationStack {
+            ZStack(){
+                
+                Color("AccentColor")
+                    .ignoresSafeArea()
+                Circle()
+                    .scale(1.7)
+                    .foregroundColor(Color(red: 255/255, green: 201/255, blue: 201/255).opacity(0.50))
+                Circle()
+                    .scale(1.35)
+                    .foregroundColor(Color(red: 255/255, green: 201/255, blue: 201/255).opacity(0.50))
             
-            
+                Circle()
+                    .scale(1.00)
+                    .foregroundColor(Color("AccentColor1").opacity(0.80))
+                
+                VStack(spacing: 30) {
+                Text("Welcome to Productivi-Me")
+                    .font(.title)
+                    .fontWeight(.black)
+                    .foregroundColor(Color("AccentColor5"))
+                    .multilineTextAlignment(.center)
+                    
+                    
+                    NavigationLink(destination: Question1view()) {
+                        Text("Start Quiz -->")
+                            .foregroundColor(Color.white)
+                            .frame(width: 150, height: 50)
+                            .background(Capsule().fill(Color("AccentColor2").opacity(0.35)))
+                    }
+                    Text("See which study method suits you the best!")
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color("AccentColor5"))
+                        .multilineTextAlignment(.center)
+                        .padding(.bottom)
+                    
+                    
+                
+                
+            }
+                .padding()
+                
+                
+            }
+
         }
-        .padding()
     }
 }
 
---
+++ Final Project/Question1view.swift
@@ -0,0 +1,24 @@
+//
+//  Question1view.swift
+//  Final Project
+//
+//  Created by Yoselin Castillo on 7/19/23.
+//
+
+import SwiftUI
+
+struct Question1view: View {
+    var body: some View {
+        ZStack(){
+            VStack(){
+                
+            }
+        }
+    }
+}
+
+struct Question1view_Previews: PreviewProvider {
+    static var previews: some View {
+        Question1view()
+    }
+}
