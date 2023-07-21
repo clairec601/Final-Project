2e442789c213b0487971cb45cb8aaa4632b5376b
Yoselin <yoselincastillo125@gmail.com>
2023-07-21 01:56:23 -0400

    I finished the design for the quiz and I added custom fonts. I might work on the design for the To-Do list and Timer in the morning

--- Final Project/Dashboard.swift
+++ Final Project/Dashboard.swift
@@ -11,95 +11,81 @@ struct Dashboard: View {
     var body: some View {
         NavigationStack{
             ZStack(){
-                
                 Color("AccentColor")
                     .ignoresSafeArea()
                 
-                
-                Rectangle()
-                    .ignoresSafeArea()
-                    .frame(width: 500, height: 200)
-                    .padding(.bottom, 700.0)
-                    .foregroundColor(Color("AccentColor4"))
+                RoundedRectangle(cornerRadius: 25)
+                    .fill(Color("AccentColor1").opacity(0.90))
+                    .frame(width: 350, height: 450)
+                   
+                    .padding(.bottom)
                 
                 RoundedRectangle(cornerRadius: 25)
-                    
-                    .frame(width: 360, height: 600)
-                    .foregroundColor(Color("AccentColor3").opacity(0.35))
-                    .padding(.top, 150.0)
+                    .fill(Color("AccentColor3").opacity(0.15))
+                    .frame(width: 380, height: 550)
+                    .padding([.leading, .bottom, .trailing])
                 
                 RoundedRectangle(cornerRadius: 25)
-                    
-                    .frame(width: 330, height: 500)
-                    .foregroundColor(Color("AccentColor3").opacity(0.20))
-                    .padding(.top, 150.0)
-                
-                VStack(alignment: .center, spacing: 20){
-                Spacer()
-                    .frame(height: 40)
-                
-                NavigationLink(destination: ToDoContentView()){
-                    Text("Set up a to-do list!")
-                }
-                .foregroundColor(Color("AccentColor5"))
-                
-                
-                NavigationLink(destination: TimerContentView()){
-                    Text("Use our work timer!")
-                }
-                .foregroundColor(Color("AccentColor5"))
-                .padding()
+                    .fill(Color("AccentColor1").opacity(0.50))
+                    .frame(width: 380, height: 650)
+                    .padding(.bottom)
                 
                 VStack{
-                    Text("Check out other study methods:")
-                        .fontWeight(.semibold)
-                        .multilineTextAlignment(.center)
-                        .padding(.horizontal, 50.0)
-                        
-                        .background(Rectangle().foregroundColor(Color("AccentColor3")))
-                    Spacer()
-                    NavigationLink(destination: ToDoContentView()){
-                            Text("Set up a to-do list!")
-                        }
-                        .foregroundColor(Color("AccentColor5"))
-                        .frame(width: 300, height: 40)
-                        .background(Capsule().foregroundColor(Color("AccentColor2")).opacity(0.35))
-                        
-                       
-                    NavigationLink(destination: TimerContentView()){
-                            Text("Use our work timer!")
-                        }
-                        .foregroundColor(Color("AccentColor5"))
-                        .foregroundColor(Color("AccentColor5"))
-                        .frame(width: 300, height: 40)
-                        .background(Capsule().foregroundColor(Color("AccentColor2")).opacity(0.35))
-                        .padding()
                     
-                    NavigationLink(destination: PomodoroMethod()){
-                        Text(" ➡ Pomodoro Method")
+                    Text("DASHBOARD")
+                    //.font(.title)
+                        .font(.custom("Caprasimo-Regular", size: 30 ))
+                    
+                        .fontWeight(.bold)
+                        .foregroundColor(Color("AccentColor5"))
+                    Spacer()
+                        .frame(height: 40)
+                    
+                    NavigationLink(destination: ToDoContentView()){
+                        Text("Set up a to-do list!")
+                    }
+                    .foregroundColor(Color("AccentColor5"))
+                    
+                    
+                    NavigationLink(destination: TimerContentView()){
+                        Text("Use our work timer!")
                     }
                     .foregroundColor(Color("AccentColor5"))
                     .padding()
                     
-                    NavigationLink(destination: FeynmanTechnique()){
-                        Text(" ➡ Feynman Technique")
+                    VStack{
+                        Text("Check out other study methods:")
+                            .font(.custom("Caprasimo-Regular", size: 24 ))
+                            .fontWeight(.semibold)
+                            .foregroundColor(Color("AccentColor5"))
+                            .padding()
+                        
+                        NavigationLink(destination: PomodoroMethod()){
+                            Text(" ➡ Pomodoro Method")
+                        }
+                        .foregroundColor(Color("AccentColor5"))
+                        .padding()
+                        
+                        NavigationLink(destination: FeynmanTechnique()){
+                            Text(" ➡ Feynman Technique")
+                        }
+                        .foregroundColor(Color("AccentColor5"))
+                        
+                        
+                        NavigationLink(destination: SpaceRepetition()){
+                            Text(" ➡ Space Repetition")
+                        }
+                        .foregroundColor(Color("AccentColor5"))
+                        .padding(.top, 10.0)
+                        .padding(.bottom, 35.0)
                     }
-                    .foregroundColor(Color("AccentColor5"))
+                    .background(Rectangle() .foregroundColor(.accentColor))
+                    .cornerRadius(15)
                     
                     
-                    NavigationLink(destination: SpaceRepetition()){
-                        Text(" ➡ Space Repetition")
-                    }
-                    .foregroundColor(Color("AccentColor5"))
-                    .padding(.top, 10.0)
-                    .padding(.bottom, 35.0)
                 }
-                .background(Rectangle() .foregroundColor(.accentColor))
-                .cornerRadius(15)
-                
-                
             }
-        }
+                }
     }
 }
 
